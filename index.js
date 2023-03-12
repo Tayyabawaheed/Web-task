9a5ca482fa3b85d244be3292092331e7fba27824
Tayyabawaheed <124914725+Tayyabawaheed@users.noreply.github.com>
2023-03-12 00:22:17 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+alert("Thanks for Watching My Document...");
+window.alert("window.alert and just alert both are a same..")
+  /////////////////////////////////////////////////////
+
+   var name = "Haleema";
+   alert("name");
+
+ /////////////////////////////////////////////////
+
+ var age = 40;
+ var newAge = age+7;
+  alert(newAge);
+
+ ////////////////////////////////////////////////////
+
+ var num = 0;
+ console.log("Number: ", num++);
+ newNum = ++num;
+ console.log("New Number: ", newNum);
+
+ var num = 10;
+ console.log("Number: ", num--);
+ newNum = --num;
+ console.log("New Number: ", newNum);
+
+///////////////////////////////////////////////////
+
+ 
+document.writeln((1+2+(8-6)*8)/(2+8*7));
+
+//////////////////////////////////////////////////
+
+
+
+ var userName = "TAYYABA";
+ var message = "Welcome ";
+ var sign = "!";
+ var greet = message+userName+sign;
+ alert(greet);
+ alert("10+7  = " + (10+7))
+
+////////////////////////////////////////////////////////
+
+ var spec = prompt("Your species?", );
+ console.log(spec)
+ 
+
+ var calc = prompt('Enter your birth year: ', '1999');
+ console.log('Your age is: ', 2023-calc);
+
+/////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
